dd1b58aee4ed34e72a8c7c43e5fef6a3508b3dfd
hrdyam <48856074+hrdyam@users.noreply.github.com>
2021-11-15 10:47:09 +0000

    added useful comments

--- add.py
+++ add.py
@@ -1,7 +1,7 @@
 def add(a,b):
-  # Added new command line!!!!
-  # return a + b
+  # if a and b are integers, return a + b
   if (type(a) == int and type(b) == int): 
     return a + b
+  # return None otherwise  
   return None
  
